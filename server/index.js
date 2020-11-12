4819a738efa1a759f55410a2c3284a8a30cfda2d
Adam Kent <adamkent@Adams-MacBook-Pro.local>
2020-11-12 11:07:36 -0700

    register endpoint

---
+++ server/index.js
@@ -0,0 +1,36 @@
+require('dotenv').config();
+
+const express = require('express');
+const massive = require('massive');
+const session = require('express-session');
+
+const {SESSION_SECRET, SERVER_PORT, CONNECTION_STRING} = process.env;
+const auth = require('./controllers/authController');
+
+const app = express();
+
+app.use(express.json());
+
+app.use(session({
+    resave: false,
+    saveUninitialized: true,
+    secret: SESSION_SECRET,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24
+    }
+}))
+
+massive({
+    connectionString: CONNECTION_STRING,
+    ssl: {
+        rejectUnauthorized: false
+    }
+}).then( db => {
+    app.set('db', db)
+    console.log('Hey! Get out of my swamp!')
+}).catch( err => console.log(err));
+
+// Auth endpoints
+app.post('/auth/register', auth.register);
+
+app.listen(SERVER_PORT, ()=>console.log(`Welcome to port ${SERVER_PORT} such a perfect town`))
